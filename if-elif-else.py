621aebee5477c65717d17e5f830a118a423a4b47
Hoshie-official <96175226+Hoshie-official@users.noreply.github.com>
2023-06-08 16:42:39 +0800

    Create if-elif-else.py
    我们可以使用if-elif-else语句来简化以上的逻辑。其中elif就是else if的意思
    特别注意: 这一系列条件判断会从上到下依次判断，如果某个判断为 True，执行完对应的代码块，后面的条件判断就直接忽略，不再执行了。

---
+++ if-elif-else.py
@@ -0,0 +1,12 @@
+# Enter a code
+age = 33
+if age >= 18:
+    print('adult')
+elif 6 <= age < 18:
+    print('teenager')
+elif 3 <= age < 6:
+    print('kid')
+elif 0 <= age < 3:
+    print('baby')
+elif age < 0:
+    print('error')
