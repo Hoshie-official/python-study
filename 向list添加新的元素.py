c304abd5bf1183a27d8e65e46016388488fe2127
Hoshie-official <96175226+Hoshie-official@users.noreply.github.com>
2023-06-13 14:19:33 +0800

    Create 向list添加新的元素.py

---
+++ 向list添加新的元素.py
@@ -0,0 +1,7 @@
+L = ['Alice', 'Bob', 'Candy', 'David', 'Ellena']
+L.append('Zero')
+L.insert(5,'Gen')
+L.insert(6,'Phoebe')
+print(L)
+#append()方法总是将元素添加到list的尾部。 
+#insert()方法需要两个参数，分别是需要插入的位置，以及需要插入的元素。
