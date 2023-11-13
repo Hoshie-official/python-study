c8ce381386eac0cd38d72c8a98699839ac5bbd16
Hoshie-official <96175226+Hoshie-official@users.noreply.github.com>
2023-11-13 13:25:18 +0800

    Update 删除dict元素.py

--- 删除dict元素.py
+++ 删除dict元素.py
@@ -24,6 +24,7 @@ else:
     print('{} not in d'.format(n))
 
 # 这里是用if来判断'Alice'是否在d.keys()里面
+# 这个in我不记得了，没想到这么简单就行……
 # 如果在的话就用d.pop()来删除这个key，这里应该是还能返回这个key的value的
 # 如果不在，这里的print语句里面也有点意思，说实话可能之前学过但我忘了
 # '{}'——这个大括号会输出Alice，应该是得益于format()
